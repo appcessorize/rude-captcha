973157dea997e58af0ade95e356327c5f7d0a740
appcessorize <mattgord@gmail.com>
2024-04-15 15:16:18 +0100

    opengraph

--- pages/captchaui.js
+++ pages/captchaui.js
@@ -270,22 +270,28 @@ export default function CapatchaUI() {
                 </div>
               </div>
             )}
-            <video
+            <img
+              src="/midfingold.png"
+              className="w-full h-full object-cover z-100"
+            />
+            {/* <video
               ref={webcamRef}
               autoPlay
               playsInline
               muted
               className="w-full h-full object-cover z-100"
-            />
-            <div className="absolute top-0 left-0 w-full h-full bg-transparent grid grid-cols-4 grid-rows-4 border border-transparent z-10">
-              {Array.from({ length: 16 }).map((_, idx) => (
-                <div
-                  key={idx}
-                  className="border border-white"
-                  style={{ minHeight: "1px" }}
-                />
-              ))}
-            </div>
+            /> */}
+            {!webcamLoading && (
+              <div className="absolute top-0 left-0 w-full h-full bg-transparent grid grid-cols-4 grid-rows-4 border border-transparent z-10">
+                {Array.from({ length: 16 }).map((_, idx) => (
+                  <div
+                    key={idx}
+                    className="border border-white"
+                    style={{ minHeight: "1px" }}
+                  />
+                ))}
+              </div>
+            )}
           </div>
 
           {/* Content block */}
@@ -333,7 +339,7 @@ export default function CapatchaUI() {
                     setStartDetection(true);
                   }}
                   type="button"
-                  className="focus:outline-none text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  className=" focus:outline-none text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                 >
                   Start
                 </button>
