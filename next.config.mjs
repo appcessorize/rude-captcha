b3d635eeec80ddbe195080082900b80b91a5be7b
appcessorize <mattgord@gmail.com>
2024-04-20 20:52:14 +0100

    Prepare gh-pages deployment

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["media.istockphoto.com", "wikipedia.org"], // Add the domains of your image sources here
-  },
-};
-
-export default nextConfig;
